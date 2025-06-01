87b2bc0759330537ef8038e7074bc22b4556e95c
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-06-01 21:58:39 +0530

    Time: 29 ms (33.53%), Space: 8.9 MB (84.71%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -6,11 +6,9 @@ public:
         for(int i = 0;i<=limit;i++){
             int rem = n - i;
          
-            if(rem<0 ||rem-2*limit>0) continue;
-            else if(rem<=limit) ans+=(rem+ 1);
-            else{
-                ans+= (long long)(2*limit -  rem + 1);
-            }
+            if(rem<0 || rem>2*limit) continue;
+           
+            ans+= ((rem<=limit) ? rem : (2*limit - rem)) + 1;
         }
 
         return ans;
