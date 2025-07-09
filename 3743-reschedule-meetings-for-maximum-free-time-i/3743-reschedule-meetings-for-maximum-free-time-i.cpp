202e41ae3e56a4014523de4a95a46514c073aa72
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-07-09 23:32:44 +0530

    Time: 0 ms (100%), Space: 125.9 MB (18.81%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& st, vector<int>& et) {
 
-        sort(st.begin(),st.end());
-        sort(et.begin(),et.end());
+       
         int n = st.size();
         int sum = 0;
         int ans = 0;
@@ -11,24 +10,21 @@ public:
         vector<int> gap;
 
         gap.push_back(st[0]);
-        for(int i = 1;i<n;i++){
-            // if(st[i]-et[i-1])
-             gap.push_back(st[i]-et[i-1]);
-        }
 
-        // if(eventTime-et[n-1])
-         gap.push_back(eventTime-et[n-1]);
+        for(int i = 1;i<n;i++)  gap.push_back(gap.back() + st[i]-et[i-1]);
+        
+        gap.push_back(gap.back() + eventTime-et[n-1]);
 
-        vector<int> pre(gap.size());
+       
 
 
         for(int i = 0;i<gap.size();i++){
-           sum+=gap[i];
+           
 
-            int curr = (i>k) ? sum - pre[i-k-1] : sum ;
+            int curr = (i>k) ? gap[i] - gap[i-k-1] : gap[i] ;
 
             ans = max(ans,curr);
-            pre[i] = sum;
+        
 
 
         }
