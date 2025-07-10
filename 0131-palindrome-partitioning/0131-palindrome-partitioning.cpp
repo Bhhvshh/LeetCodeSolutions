52328c8e482c8da295db91f47c3ffefd6e595a1f
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-07-10 22:53:22 +0530

    Time: 24 ms (97.55%), Space: 53.4 MB (93.62%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-bool pal(string &s){
-    for(int l = 0, r= s.size() - 1;l<r;l++,r--){
-        if(s[l]!=s[r]) return false;
+// bool pal(string &s){
+//     for(int l = 0, r= s.size() - 1;l<r;l++,r--){
+//         if(s[l]!=s[r]) return false;
 
-    }
+//     }
      
 
-    return true;
+//     return true;
 
-}
+// }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> curr;
-        backtrack(0,ans,curr,s);
+        vector<vector<bool>> dp(s.size(),vector<bool> (s.size()));
+        for(int i = 0;i<s.size();i++){
+            for(int j = 0;j<=i;j++){
+                if(i==j) dp[j][i] = 1;
+                else if(s[i]!=s[j]) dp[j][i] = false;
+                else dp[j][i] =(i==j+1) ? true: dp[j+1][i-1];
+
+            }
+        }
+        backtrack(0,ans,curr,s,dp);
 
         return ans;
         
     }
 
 
-    void backtrack(int id,vector<vector<string>> &ans,vector<string> &curr,string &s){
+    void backtrack(int id,vector<vector<string>> &ans,vector<string> &curr,string &s,vector<vector<bool>> &dp){
         if(id==s.size()){
             ans.push_back(curr);
         }
-        string tmp ="";
+        
         for(int i = id;i<s.size();i++){
-            tmp+=s[i];
-            if(pal(tmp)){
-                curr.push_back(tmp);
-                backtrack(i+1,ans,curr,s);
+            
+            if(dp[id][i]){
+                curr.push_back(s.substr(id,i-id+1));
+                backtrack(i+1,ans,curr,s,dp);
                 curr.pop_back();
             }
         }
