1ef07b016a35c461257b0a25c85419f855035766
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-07-16 16:16:27 +0530

    Time: 5 ms (59.12%), Space: 96.2 MB (62.35%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -3,38 +3,17 @@ public:
     int maximumLength(vector<int>& nums) {
         int even = 0;
         int odd = 0;
+        int starteven = 0, startodd = 0;
+        int flag1 = 0 ,flag2 = 1;
         for(auto x:nums){
             odd += (x%2);
             even += (x%2==0);
+            if(x%2==flag1) {starteven++; flag1 = !flag1;}
+            if(x%2==flag2){startodd++; flag2 = !flag2;}
+
         }
 
         int ans = max(odd,even);
-
-        int starteven = 0, startodd = 0;
-        bool flag = true, flag2 = true;
-        
-        for(int i = 0;i<nums.size();i++){
-            if(flag && (nums[i]%2==0)){ 
-                starteven++;
-                flag = !flag;
-
-            }
-            else if(!flag && (nums[i]%2)){
-                starteven++;
-                flag = !flag;
-            }
-
-            if(flag2 && (nums[i]%2)){ 
-                startodd++;
-                flag2 = !flag2;
-
-            }
-            else if(!flag2 && (nums[i]%2==0)){
-                startodd++;
-                flag2 = !flag2;
-            }
-        }
-
 
         return max(ans,max(starteven,startodd));
     }
