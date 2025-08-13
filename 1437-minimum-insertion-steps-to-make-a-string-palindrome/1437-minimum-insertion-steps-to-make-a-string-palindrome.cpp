491170074627255aa95c4ee485166832defd810f
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-08-13 16:45:20 +0530

    Time: 35 ms (49.14%), Space: 31.5 MB (37.41%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int>> memo(n,vector<int> (n,-1));
+
+        
+
+        return helper(s,0,n-1,memo);
+        
+    }
+    int helper(string &s,int l, int r,vector<vector<int>>&memo){
+        if(l>=r) return 0;
+        if(memo[l][r]!=-1) return memo[l][r];
+
+
+        if(s[l]==s[r]){
+            return memo[l][r] = helper(s,l+1,r-1,memo);
+        }
+        return memo[l][r] = 1 + min(helper(s,l+1,r,memo),helper(s,l,r-1,memo));
+
+    }
+};
