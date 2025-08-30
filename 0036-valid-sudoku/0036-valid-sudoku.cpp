659bfe35e6f023304c0c126e809fa1df4b5439a5
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-08-30 13:44:38 +0530

    Time: 2 ms (66.4%), Space: 23.4 MB (58.58%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> row(9,vector<bool>(10,false)), col(9,vector<bool>(10,false));
+        vector<vector<vector<bool>>>box(3,vector<vector<bool>>(3,vector<bool>(10,false)));
+
+        for(int i = 0;i<9;i++){
+            for(int j = 0;j<9;j++){
+
+                if(board[i][j]!='.' && (row[i][board[i][j]-'0'] || col[j][board[i][j]-'0'] || box[i/3][j/3][board[i][j]-'0'])){
+                return false;
+            }
+            else if(board[i][j]!='.') {
+                row[i][board[i][j]-'0'] =true;
+                 col[j][board[i][j]-'0'] =true;
+                 box[i/3][j/3][board[i][j]-'0'] = true;
+            }
+            }
+        }
+
+        return true;
+    }
+};
