876ff52150e4c6125aa8627ae4a2addb15dc5be3
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2026-08-03 22:49:47 +0530

    Time: 82 ms (17.14%), Space: 154.8 MB (15.06%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,60 +1,35 @@
 class Solution {
+    static inline const vector<string> names = {"Bob", "Alice"};
+
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
 
-        vector<vector<pair<int,int>>> dp(2,vector<pair<int,int>> (n, {INT_MAX,INT_MIN}));
+        vector<vector<int>> dp(2,vector<int> (n, INT_MIN));
 
         auto ans = helper(0, 0, stoneValue, dp);
 
-        if(ans.first == ans.second) return "Tie";
-        vector<string> names = {"Bob", "Alice"};
+        if(ans == 0) return "Tie";
 
 
-        return names[(ans.first > ans.second)];
+        return names[ans > 0];
     }
 
 
-    pair<int,int> helper(int id ,bool turn, vector<int> &stoneVal, vector<vector<pair<int,int>>> &dp){
+    int helper(int id ,bool turn, vector<int> &stoneVal, vector<vector<int>> &dp){
 
-        if(id == stoneVal.size()) return {0,0};
-        if(dp[turn][id] != std::make_pair(INT_MAX, INT_MIN)) return dp[turn][id];
+        if(id == stoneVal.size()) return 0;
+        if(dp[turn][id] != INT_MIN) return dp[turn][id];
 
-        pair<int,int> ans;
         int diff = INT_MIN;
-
-
 
         for(int i = id, curr = 0; i < min(id + 3, (int)stoneVal.size()) ; i++){
 
-            pair<int,int> tmp = helper(i + 1, !turn, stoneVal, dp);
             curr+=stoneVal[i];
-            if(turn && (curr + tmp.second - tmp.first) > diff ){
-                tmp.second += curr;
-                ans = tmp;
-                diff = tmp.second - tmp.first;
-                
-            }
-            else if(!turn && (curr + tmp.first - tmp.second) > diff){
-                tmp.first += curr;
-                ans = tmp;
-                diff = tmp.first - tmp.second;
-                // cout << "Alice" << " "<< diff << endl;
-
-            }
-
-
-
-
-
-
-
+            diff = max(diff, curr - helper(i + 1, !turn, stoneVal, dp));
             
         }
 
-         
-
-
-        return dp[turn][id] = ans;
+        return dp[turn][id] = diff;
     }
 };
